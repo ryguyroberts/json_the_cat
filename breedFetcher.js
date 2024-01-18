e993516de62ea8175e45a7c66ec72efef9858f7f
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-18 09:41:59 -0800

    Added breedFether test file and small refactoring so callback is handling two parameters correctly

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,15 +3,15 @@ const request = require('request');
 const fetchBreedDescription = (breed, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
     if (error) {
-      return callback(error);
+      return callback(error, response);
     }
     // Returns string with [] breed name bad
     if (body === "[]") {
-      return callback("Something wrong with Breed name");
+      return callback(error, `Something wrong with Breed name ${breed}.`);
     }
     // No errors and breed correct return body.description
     const data = JSON.parse(body);
-    return callback(data[0].description);
+    return callback(error, data[0].description);
   });
 };
 
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const { fetchBreedDescription } = require('./breedFetcher');
 const breedName = process.argv[2];
 
-// Quick cs function
-const logOutput = (element) => {
-  console.log(element);
+// Quick cs function for testing
+const logOutput = (element, element2) => {
+  // If no error we only only interested in element 2 the decriptions
+  if (element === null) {
+    return console.log(element2);
+  }
+  return console.log(element, element2);
 };
 
 fetchBreedDescription(breedName, logOutput);
